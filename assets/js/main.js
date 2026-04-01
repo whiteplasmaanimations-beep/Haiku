d45ad5131c3eb82843c7a206653c7f4fcf578a1b
whiteplasmaanimations-beep <whiteplasmaanimations@gmail.com>
2026-03-31 20:04:54 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,35 @@
 // main.js
+let grid = document.querySelectorAll(".grid-container");
+let divs = document.querySelectorAll(".grid-container div"); 
 
-console.log("Hello, World!");
+function randomize() {
+  console.log(divs.length, "randomize!"); 
+  //affects the whole grid
+  grid[0].style.transform = 'rotate(${Math.random() * 360}deg)';
+
+  // just the grid divs
+  let scale = randomNumber(.5, 1.2); 
+  //bruh
+  let translateX = randomNumber(0, 50); 
+  let translateY = randomNumber(0,20); 
+  let rotate = randomNumber(0,360);
+
+  // 50% of the time 
+  if (Math.random() > .5) {
+    scale = 1; // don't affect scale 
+    translateX = randomNumber(0,80);
+    translateY = randomNumber(0, 80);
+    
+  }
+  div.style.transform = 'scale(${scale}) translate(${translateX}%,${translateY}%) rotate(${rotate}deg)';
+
+  
+}
+
+  document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
+/*console.log("Hello, World!");*/
