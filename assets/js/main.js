4d8c46c43040cd06a11fcd929dea76493861a41c
whiteplasmaanimations-beep <whiteplasmaanimations@gmail.com>
2026-03-31 20:31:19 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ function randomize() {
     translateY = randomNumber(0, 80);
     
   }
-  div.style.transform = 'scale(${scale}) translate(${translateX}%,${translateY}%) rotate(${rotate}deg)';
+  div.style.transform = `scale(${scale}) translate(${translateX}%,${translateY}%) rotate(${rotate}deg)`;
   }); 
   
 }
